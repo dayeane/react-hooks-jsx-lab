9d73507f7dd210e197001dbef05022153ce7a3c5
Dayeane <dayeane85@gmail.com>
2021-10-31 15:27:29 -0700

    commit tree cb6e59e74bb9214e2c8ddd913fdb56c286bf5ced

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (<div id="about">
+    <h2>About Me</h2>
+    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et </p>
+    <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this"/> 
+  </div>);
 }
 
 export default About;
